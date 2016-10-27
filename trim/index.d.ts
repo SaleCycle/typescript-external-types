11cf8c85927f5ad984f77f3f67bf340d11ef98dc
Steve Higgs <steve.higgs@salecycle.com>
2016-10-27 13:29:59 +0100

    more types taken from the core typescript definitions

--- trim/index.d.ts
+++ trim/index.d.ts
@@ -1,5 +1,6 @@
 
 declare module 'trim' {
+  /** Removes the leading and trailing white space and line terminator characters from a string. */
   function trim(value: string): string;
   export = trim;
 }
